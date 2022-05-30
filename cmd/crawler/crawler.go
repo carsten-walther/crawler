4d290c02ded62b5c4277cdc99ba5a7dad3a0de82
Carsten Walther <walther.carsten@web.de>
2022-05-30 14:21:25 +0200

    Feat: add console coloring on verbose output, code improvements

--- cmd/crawler/crawler.go
+++ cmd/crawler/crawler.go
@@ -8,26 +8,23 @@ import (
 	"os"
 	"time"
 
+	"github.com/TwiN/go-color"
+
 	sitemap "github.com/oxffaa/gopher-parse-sitemap"
 )
+
+var url string
+var output string
+var count int
+var verbose bool
 
 func main() {
 
 	var file *os.File
-	var urls []string
-	var record []string
-	var currentCount int
 
-	var url string
 	flag.StringVar(&url, "url", "", "URL to crawl, eg. https://www.your-website.com/sitemap.xml")
-
-	var output string
 	flag.StringVar(&output, "output", "", "Path to results csv file.")
-
-	var count int
 	flag.IntVar(&count, "count", 0, "Maximum count of urls to crawl.")
-
-	var verbose bool
 	flag.BoolVar(&verbose, "verbose", false, "Show verbose output.")
 
 	flag.Parse()
@@ -43,29 +40,32 @@ func main() {
 		writeFile([]string{"id", "datetime", "state", "url"}, file)
 	}
 
-	verboseOutput("Starting ...", verbose)
+	verboseOutput("> Fetching URLs ...")
 
-	urls = collectUrls(url)
-	currentCount = 1
+	urls := collectUrls(url)
+
+	verboseOutput(fmt.Sprintf("> %v URLs found ...\n", len(urls)))
+
+	currentCount := 0
 
 	for _, url := range urls {
+		currentCount++
 		if count <= 0 {
-			record = callUrl(currentCount, url, verbose)
+			record := callUrl(currentCount, url, len(urls))
 			if output != "" {
 				writeFile(record, file)
 			}
 		} else {
 			if currentCount <= count {
-				record = callUrl(currentCount, url, verbose)
+				record := callUrl(currentCount, url, len(urls))
 				if output != "" {
 					writeFile(record, file)
 				}
 			}
 		}
-		currentCount++
 	}
 
-	verboseOutput("Done ...", verbose)
+	verboseOutput("\n> Done ...")
 }
 
 /* network functions */
@@ -75,20 +75,10 @@ func collectUrls(source string) []string {
 	resultIndex := getIndex(source)
 	if len(resultIndex) > 0 {
 		for _, index := range resultIndex {
-			resultEndpoints := getEndpoint(index)
-			if len(resultEndpoints) > 0 {
-				for _, url := range resultEndpoints {
-					urls = append(urls, url)
-				}
-			}
+			urls = append(urls, getEndpoint(index)...)
 		}
 	} else {
-		resultEndpoints := getEndpoint(source)
-		if len(resultEndpoints) > 0 {
-			for _, url := range resultEndpoints {
-				urls = append(urls, url)
-			}
-		}
+		urls = append(urls, getEndpoint(source)...)
 	}
 	return urls
 }
@@ -119,21 +109,34 @@ func getEndpoint(url string) []string {
 	return result
 }
 
-func callUrl(count int, url string, verbose bool) []string {
+func callUrl(count int, url string, index int) []string {
 	response, err := http.Get(url)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-	verboseOutput(fmt.Sprintf("%v, %v : %s", count, response.StatusCode, url), verbose)
+
+	message := fmt.Sprintf("%s, %s, %v, %s", lpad(fmt.Sprint(count), " ", recursionCountDigits(index)), time.Now().Format(time.UnixDate), response.StatusCode, url)
+
+	switch response.StatusCode {
+	case 200:
+		verboseOutput(color.Colorize(color.Green, message))
+	case 404:
+		verboseOutput(color.Colorize(color.Red, message))
+	case 500:
+		verboseOutput(color.Colorize(color.Red, message))
+	default:
+		verboseOutput(message)
+	}
+
 	return []string{fmt.Sprintf("%v", count), time.Now().Format(time.UnixDate), fmt.Sprintf("%v", response.StatusCode), url}
 }
 
 /* verbose functions */
 
-func verboseOutput(message string, verbose bool) {
-	if verbose == true {
-		fmt.Println(fmt.Sprintf("# %s : %s", time.Now().Format(time.UnixDate), message))
+func verboseOutput(message string) {
+	if verbose {
+		fmt.Println(message)
 	}
 }
 
@@ -164,4 +167,21 @@ func closeFile(f *os.File) {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+/* tool functions */
+
+func recursionCountDigits(number int) int {
+	if number < 10 {
+		return 1
+	} else {
+		return 1 + recursionCountDigits(number/10)
+	}
+}
+
+func lpad(s string, pad string, plength int) string {
+	for i := len(s); i < plength; i++ {
+		s = pad + s
+	}
+	return s
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,7 @@
 module github.com/carsten-walther/crawler
 
-require (
-    github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
-)
+require github.com/oxffaa/gopher-parse-sitemap v0.0.0-20191021113419-005d2eb1def4
+
+require github.com/TwiN/go-color v1.1.0 // indirect
 
 go 1.18
